f373a117a1287a2c2731a26af886aa668f3dd305
Rizza Marwan <rizzamarwan@icloud.com>
2024-02-12 02:14:23 +0700

    Update

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import profilePicture from './images/profile-picture.jpeg'
+import profilePicture from './src/images/profile-picture.jpeg'
 
 function PhotoFoto() {
     return (
